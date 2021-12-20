972050ec8c6eed2511d181c579a52ab7b63bea65
Erik <braniubojni@mail.ru>
2021-12-21 00:13:53 +0400

    continue on 37.37, mp3 and picture added succesfully

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
+import { FileModule } from "./file/file.module";
 import { TrackModule } from "./track/track.module";
 
 @Module({
   imports: [
     TrackModule,
+    FileModule,
     MongooseModule.forRoot(
       "mongodb+srv://admin:44sh5hrQ4gFR9y4@cluster0.siwym.mongodb.net/music-platform?retryWrites=true&w=majority"
     ),
